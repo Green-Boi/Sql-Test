b468a5e9ddecb84d236dada77f78de87870d1dd2
Green-Boi <rafeekamary456@gmail.com>
2022-04-17 23:42:51 +0300

    sql testing in c#

---
+++ SqlTest/SqlTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace SqlTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ok");
+            string connectionString = @"Data Source=SqlTest.db";
+            using (var connection = new SqliteConnection(connectionString))
+            {
+
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS drinking_water (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+                
+
+            }
+
+        
+        }
+    }
+}
